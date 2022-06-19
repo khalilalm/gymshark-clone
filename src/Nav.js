97c39a8c08bfae3c50e92fe25dd228be95fdf9fa
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2022-06-19 06:47:08 -0400

    bug fixes

--- src/Nav.js
+++ src/Nav.js
@@ -8,20 +8,22 @@ const Nav = () => {
     return (
         <div className="nav-content">
             <div className="nav-info">
+                
                 <ul className="nav-info-content">
-                  <li><img src={profile} className="profile-img"/> My Account</li>
+                    <img src={logo} className="logo"/>
+                  <li><span></span>My Account</li>
                     <li>Blog</li>
                     <li>Newsletter</li>
                     <li>Help</li>
-                    <li><img src={us}className="us-logo"/>United States </li>
+                    <li>United States </li>
                 </ul>
             </div>
-            <nav>
-                <ul className="nav" > 
-                    <img src={logo} className="logo"/>
-                    <li><a>WOMENS</a></li>
-                    <li><a>MENS</a></li>
-                    /<li></li>
+            <nav className="nav">
+                <ul > 
+                    
+                    <li><a>WOMEN'S</a></li>
+                    <li><a>MEN'S</a></li>
+                    <li></li>
                     <li></li>
                     <li></li>
                    
